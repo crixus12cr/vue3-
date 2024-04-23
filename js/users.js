78818b28d195ba9349c46f60fd416f6506881a4a
cperdomo12cr <cperdomo@arcetec.com.co>
2024-04-22 22:12:14 -0500

    feat: avanzando con el crud

---
+++ js/users.js
@@ -0,0 +1,33 @@
+window.addEventListener('load', ()=>{
+    const app=Vue.createApp({
+        data(){
+            return{
+                users:[],
+               
+            }
+        },
+        created() {
+            if(localStorage.getItem('vue3.users')){
+                this.users=JSON.parse(localStorage.getItem('vue3.users'));
+            } else {
+                this.listUsers();
+            }
+
+            this.listUsers();
+        },
+        methods: {
+            listUsers: async function(){
+                const res= await fetch('https://jsonplaceholder.typicode.com/users');
+                const data= await res.json();
+                this.users=data;
+                this.updateLocalStorage();
+                console.log(data);
+            },
+            updateLocalStorage: function(){
+                localStorage.setItem('vue3.users', JSON.stringify(this.users));
+            }
+        },
+    });
+
+    app.mount('#app');
+});
